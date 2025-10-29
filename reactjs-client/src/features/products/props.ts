749b087d1877c6acc899eb49c11bc5f004d5b7d5
Lucas-coincodes0313 <lucas.coincodes0313@gmail.com>
2025-10-29 00:22:26 -0400

    fix #1: add delete func
    Co-authored-by: Christian Lee Richardson <christianrichardson.finance@gmail.com>

--- reactjs-client/src/features/products/props.ts
+++ reactjs-client/src/features/products/props.ts
@@ -3,4 +3,5 @@ export interface IProductListProps extends IProductStoreState {
     getProductList(): Promise<void>;
     createProduct(productModel: IProductCreate): Promise<void>;
     updateProduct(productId: string, productModel: IProductUpdate): Promise<void>;
+    deleteProduct(productId: string): Promise<void>;
 }
